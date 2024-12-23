85476b2fa99a42ed41d454713cc9405a36bb616b
mamatharaju850 <rajumamatha850@gmail.com>
2024-12-23 15:51:53 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,18 @@
+javascript
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.use('/api/users', require('./routes/userRoutes'));
+app.use('/api/transactions', require('./routes/transactionRoutes'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
